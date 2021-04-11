5b72e92d033938945390ad645cc60dad2204920b
Focus04 <alexogrezeanu2004@gmail.com>
2021-04-11 23:44:36 +0300

    .

--- commands/serverchart.js
+++ commands/serverchart.js
@@ -37,7 +37,7 @@ module.exports = {
         chartJS.plugins.register(
           {
             beforeDraw: (chartInstance) => {
-              chartInstance.chart.ctx.fillStyle = '#ffffff';
+              chartInstance.chart.ctx.fillStyle = 'white';
               chartInstance.chart.ctx.fillRect(0, 0, chartInstance.chart.width, chartInstance.chart.height);
             }
           }
