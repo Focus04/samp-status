b9680f02b0305a1967e93653751a38cb314da107
Focus04 <alexogrezeanu2004@gmail.com>
2021-04-26 20:27:00 +0300

    .

--- commands/status.js
+++ commands/status.js
@@ -20,60 +20,9 @@ module.exports = {
       return message.react(reactionError);
     }
     
-    let err = 0;
-    const data = await gamedig.query({
-      type: 'samp',
-      host: server.ip,
-      port: server.port,
-      maxAttempts: 10
-    }).catch(async (error) => err = 0);
-    if (err === 1 || !data) {
-      let msg = await loading.edit(`${server.ip}:${server.port} did not respond after 10 attempts.`);
-      msg.delete({ timeout: deletionTimeout });
-      return message.react(reactionError);
-    }
-
-    const config = {
-      border: getBorderCharacters(`void`),
-      columnDefault: {
-        paddingLeft: 0,
-        paddingRight: 1
-      },
-      drawHorizontalLine: () => {
-        return false
-      }
-    }
-    let players = [];
-    data.players.forEach(player => {
-      let p = [];
-      p[0] = player.id;
-      p[1] = player.name;
-      p[2] = player.score;
-      p[3] = player.ping;
-      players.push(p);
-    });
-    let output;
-    if (!players.length) output = 'None';
-    else output = table(players, config);
-    let color;
-    if (message.guild.me.roles.highest.color === 0) color = '#b9bbbe';
-    else color = message.guild.me.roles.highest.color;
-    let serverEmbed = new MessageEmbed()
-      .setColor(color)
-      .setTitle(`${data.name}`)
-      .setDescription(data.raw.gamemode)
-      .addFields(
-        { name: 'Server IP', value: `${server.ip}:${server.port}`, inline: true },
-        { name: 'Map', value: `${data.raw.rules.mapname}`, inline: true },
-        { name: 'Time', value: `${data.raw.rules.worldtime}`, inline: true },
-        { name: 'Forums', value: 'http://' + data.raw.rules.weburl, inline: true },
-        { name: 'Version', value: `${data.raw.rules.version}`, inline: true },
-        { name: 'Players', value: `${data.players.length}/${data.maxplayers}`, inline: true }
-      )
-      .setTimestamp();
-    if (data.players.length > 0) serverEmbed.addField('ID Name Score Ping', '```' + output + '```');
+    const status = await getStatus(server, MessageEmbed, getBorderCharacters, gamedig, table);
     await loading.delete();
-    await message.channel.send(serverEmbed);
+    await message.channel.send(status);
     message.react(reactionSuccess);
   }
 }
--- features/status.js
+++ features/status.js
@@ -1,5 +1,55 @@
 module.exports = {
-  getStatus: (guildID) => {
+  getStatus: (server, MessageEmbed, getBorderCharacters, gamedig, table) => {
+    let err = 0;
+    const data = await gamedig.query({
+      type: 'samp',
+      host: server.ip,
+      port: server.port,
+      maxAttempts: 10
+    }).catch(() => err = 0);
+    if (err === 1 || !data) {
+      return `${server.ip}:${server.port} did not respond after 10 attempts.`;
+    }
 
+    const config = {
+      border: getBorderCharacters(`void`),
+      columnDefault: {
+        paddingLeft: 0,
+        paddingRight: 1
+      },
+      drawHorizontalLine: () => {
+        return false
+      }
+    }
+    let players = [];
+    data.players.forEach(player => {
+      let p = [];
+      p[0] = player.id;
+      p[1] = player.name;
+      p[2] = player.score;
+      p[3] = player.ping;
+      players.push(p);
+    });
+    let output;
+    if (!players.length) output = 'None';
+    else output = table(players, config);
+    let color;
+    if (message.guild.me.roles.highest.color === 0) color = '#b9bbbe';
+    else color = message.guild.me.roles.highest.color;
+    let serverEmbed = new MessageEmbed()
+      .setColor(color)
+      .setTitle(`${data.name}`)
+      .setDescription(data.raw.gamemode)
+      .addFields(
+        { name: 'Server IP', value: `${server.ip}:${server.port}`, inline: true },
+        { name: 'Map', value: `${data.raw.rules.mapname}`, inline: true },
+        { name: 'Time', value: `${data.raw.rules.worldtime}`, inline: true },
+        { name: 'Forums', value: 'http://' + data.raw.rules.weburl, inline: true },
+        { name: 'Version', value: `${data.raw.rules.version}`, inline: true },
+        { name: 'Players', value: `${data.players.length}/${data.maxplayers}`, inline: true }
+      )
+      .setTimestamp();
+    if (data.players.length > 0) serverEmbed.addField('ID Name Score Ping', '```' + output + '```');
+    return serverEmbed;
   }
 }
