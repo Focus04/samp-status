8ba211bacf3cd5cb430ff4d006060226f538afa2
Focus04 <alexogrezeanu2004@gmail.com>
2021-04-26 20:59:06 +0300

    Bot now deletes old charts when sending a new one

--- events/init/ready.js
+++ events/init/ready.js
@@ -46,6 +46,8 @@ module.exports = async (client) => {
         if (data.days.length > 30) data.days.shift();
         const channel = guild.channels.cache.get(interval.channel);
         const chart = await getChart(data, guild, ChartJSNodeCanvas, MessageAttachment, moment);
+        const oldMsg = await channel.messages.fetch(data.msg).catch((err) => console.log(err));
+        if (oldMsg) oldMsg.delete();
         const msg = channel.send(chart);
         data.msg = msg;
         await maxPlayers.set(`${serverAddress.ip}:${serverAddress.port}`, data);
