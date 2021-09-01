c39e72773eea69d02ba0dc4a3e5313ad6b9c81eb
Focus <alexogrezeanu2004@gmail.com>
2021-09-01 20:18:35 +0300

    .

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 require('dotenv').config();
-const { readdirSync, cp } = require('fs');
-const { Client } = require('discord.js');
+const { readdirSync } = require('fs');
+const { Client, Collection } = require('discord.js');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
 
 const client = new Client({ intents: ['GUILD_MESSAGES', 'GUILDS'] });
 
 let commands = [];
+client.commands = new Collection();
 readdirSync('./commands').forEach((file) => {
   const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
   commands.push(command.data.toJSON());
 });
 const rest = new REST({ version: '9' }).setToken(process.env.token);
