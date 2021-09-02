555a7e2ef8c0e7779667c3196d76f527e3594529
Focus <alexogrezeanu2004@gmail.com>
2021-09-02 16:42:50 +0300

    .

--- commands/serverchart.js
+++ commands/serverchart.js
@@ -10,17 +10,12 @@ module.exports = {
     .setName('serverchart')
     .setDescription('Sends a chart displaying server statistics for each day.'),
   async execute(interaction) {
-    let loadingMsg = await message.channel.send('Fetching server info...');
-    const interval = await intervals.get(message.guild.id);
-    if (!interval) {
-      let msg = await loadingMsg.edit(`You must set an interval to view statistics. Set one using /setinterval`);
-    }
-
-    const { server } = message.client.guildConfigs.get(message.guild.id);
+    const interval = await intervals.get(interaction.guildId);
+    if (!interval) await loadingMsg.edit(`You must set an interval to view statistics. Set one using /setinterval`);
+    const { server } = interaction.client.guildConfigs.get(interaction.guildId);
     const data = await maxPlayers.get(`${server.ip}:${server.port}`);
-    const color = getRoleColor(message.guild);
+    const color = getRoleColor(interaction.guild);
     const attachment = await getChart(data, color);
-    await loadingMsg.delete();
-    await message.channel.send(attachment);
+    await interaction.reply(attachment);
   }
 }
