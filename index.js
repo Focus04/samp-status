ace5266d451ef6a2a350b66e6561d68e3f7ff52a
Focus <alexogrezeanu2004@gmail.com>
2021-09-02 23:12:48 +0300

    .

--- index.js
+++ index.js
@@ -7,28 +7,23 @@ const { clientId } = require('./config.json');
 
 const client = new Client({ intents: ['GUILD_MESSAGES', 'GUILDS'] });
 
-const loadCommands = (async () => {
-  let commands = [];
-  client.commands = new Collection();
-  readdirSync('./commands').forEach((file) => {
-    const command = require(`./commands/${file}`);
-    client.commands.set(command.data.name, command);
-    commands.push(command.data.toJSON());
-  });
-  const rest = new REST({ version: '9' }).setToken(process.env.token);
-  await rest.put(
-    Routes.applicationCommands(clientId, '729313166835712033'),
-    { body: [] }
-  );
-})();
+let commands = [];
+client.commands = new Collection();
+readdirSync('./commands').forEach((file) => {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+  commands.push(command.data.toJSON());
+});
+const rest = new REST({ version: '9' }).setToken(process.env.token);
+client.guilds.forEach(async (guild) => {
+  await rest.put(Routes.applicationGuildCommands(clientId, guild.id), { body: commands });
+});
 
-const loadEvents = (() => {
-  readdirSync('./events').forEach((folder) => {
-    readdirSync(`./events/${folder}`).forEach((file) => {
-      const event = require(`./events/${folder}/${file}`);
-      client.on(file.split('.')[0], event.bind(null, client));
-    });
+readdirSync('./events').forEach((folder) => {
+  readdirSync(`./events/${folder}`).forEach((file) => {
+    const event = require(`./events/${folder}/${file}`);
+    client.on(file.split('.')[0], event.bind(null, client));
   });
-})();
+});
 
 client.login(process.env.token);
