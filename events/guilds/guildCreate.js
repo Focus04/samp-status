1ed9a270ba0eb23ef8aaf61ff8f2a6f640171e45
Focus <alexogrezeanu2004@gmail.com>
2021-09-03 14:25:35 +0300

    .

--- events/guilds/guildCreate.js
+++ events/guilds/guildCreate.js
@@ -1,5 +1,5 @@
 const commands = require('../../index');
 
 module.exports = async (client, guild) => {
-  await client.application.commands.set(commands, guild.id);
+  await client.application.commands.set(commands, guild.id).catch((err) => console.log(err));
 }
--- events/init/ready.js
+++ events/init/ready.js
@@ -13,7 +13,9 @@ const maxPlayers = new Keyv(process.env.maxPlayers);
 module.exports = async (client) => {
   console.log('I am live');
   client.user.setActivity('SA:MP');
-  client.guilds.cache.forEach(async (guild) => await client.application.commands.set(commands, guild.id));
+  client.guilds.cache.forEach(async (guild) => {
+    await client.application.commands.set(commands, guild.id).catch((err) => console.log(err));
+  });
   client.guildConfigs = new Collection();
   client.guilds.cache.forEach(async (guild) => {
     let prefix = await prefixes.get(guild.id);
