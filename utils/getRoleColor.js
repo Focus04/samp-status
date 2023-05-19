77b86970d76ef82f48cac17a984109f6cd2820da
Ogrezeanu Alexandru <alexogrezeanu2004@gmail.com>
2023-05-19 15:40:53 +0300

    .

--- utils/getRoleColor.js
+++ utils/getRoleColor.js
@@ -1,16 +1,19 @@
 export function getRoleColor(guild) {
-  let roleHexColor = '';
-  if (!guild.members.me.roles.color)
-    roleHexColor = '#5865F2';
-  else
-    roleHexColor = guild.members.me.roles.color.color;
+  let highestRole = guild.members.me.roles.color;
+  let roleHexColor, roleColor;
+  if (!highestRole) roleHexColor = roleColor = '#5865F2';
+  else {
+    roleHexColor = '#' + highestRole.color.toString(16);
+    roleColor = highestRole.color;
+  }
   let r = parseInt(roleHexColor.slice(1, 3), 16);
   let g = parseInt(roleHexColor.slice(3, 5), 16);
   let b = parseInt(roleHexColor.slice(5, 7), 16);
+
   let color = {
     rgb: `rgb(${r}, ${g}, ${b})`,
     rgba: `rgba(${r}, ${g}, ${b}, 0.1)`,
-    hex: roleHexColor
+    hex: roleColor
   };
   return color;
 }
