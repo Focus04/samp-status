1f0417de9fa7b1fe1fc21a6fa9d575c7ef8e1967
Ogrezeanu Alexandru <alexogrezeanu2004@gmail.com>
2023-05-19 15:05:31 +0300

    Added /botinfo

---
+++ commands/botinfo.js
@@ -0,0 +1,22 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const { getRoleColor } = require('../utils/getRoleColor');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('botinfo')
+    .setDescription(`Checks how many servers the bot is in.`),
+  async execute(interaction) {
+    let color = getRoleColor(interaction.guild);
+    let membercount = 0;
+    interaction.client.guilds.cache.forEach((guild) => membercount += guild.memberCount);
+    const infoEmbed = new EmbedBuilder()
+      .setColor(color)
+      .setTitle('Bot info')
+      .addFields(
+        { name: 'Server Count', value: interaction.client.guilds.cache.size.toString() },
+        { name: 'User Count', value: membercount.toString() }
+      )
+      .setTimestamp();
+    interaction.reply({ embeds: [infoEmbed] });
+  }
+}
