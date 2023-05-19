ef0703b90bdc0020670df578aa5d5a4f90109dce
Ogrezeanu Alexandru <alexogrezeanu2004@gmail.com>
2023-05-19 14:31:56 +0300

    .

--- utils/getRoleColor.js
+++ utils/getRoleColor.js
@@ -1,15 +1,13 @@
 export function getRoleColor(guild) {
-  let roleColor = guild.members.me.roles.color.color;
-  let roleHexColor = '';
+  if (!guild.members.me.roles.color)
+    roleHexColor = '#5865F2';
+  else
+    roleHexColor = '#' + guild.members.me.roles.color.color.toString(16);
   // let highestRole = { position: -1 };
   // guild.members.me.roles.cache.forEach((role) => {
   //   if (role.position > highestRole.position && role.color != 0)
   //     highestRole = role;
   // });
-  if (roleColor === -1)
-    roleHexColor = '#5865F2';
-  else
-    roleHexColor = '#' + highestRole.color.toString(16);
   let r = parseInt(roleHexColor.slice(1, 3), 16);
   let g = parseInt(roleHexColor.slice(3, 5), 16);
   let b = parseInt(roleHexColor.slice(5, 7), 16);
