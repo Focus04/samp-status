d31a19eed5db948e5a4d88656426da901ddbcbad
Ogrezeanu Alexandru <alexogrezeanu2004@gmail.com>
2023-07-12 18:33:36 +0300

    .

--- utils/getStatus.js
+++ utils/getStatus.js
@@ -18,8 +18,6 @@ export async function getStatus(server, color) {
       .setTimestamp();
     return errEmbed;
   }
-  console.log(`Name: ${data.players[1].name}`);
-  console.log(data.players[1].raw);
 
   const config = {
     border: getBorderCharacters(`void`),
