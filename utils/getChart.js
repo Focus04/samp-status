ee48db294464e958333471749c0a8c171cf57c93
Ogrezeanu Alexandru <alexogrezeanu2004@gmail.com>
2023-08-01 20:18:29 +0300

    .

--- utils/getChart.js
+++ utils/getChart.js
@@ -11,8 +11,8 @@ export async function getChart(data, color) {
   });
 
   const canvas = new ChartJSNodeCanvas({
-    width: 1920,
-    height: 1080
+    width: 1280,
+    height: 720
   });
 
   const config = {
