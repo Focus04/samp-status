e2cdabb41b33833bd819b11b23fa0c0eeb1fa08c
Ogrezeanu Alexandru <alexogrezeanu2004@gmail.com>
2023-08-01 22:06:53 +0300

    .

--- utils/getChart.js
+++ utils/getChart.js
@@ -45,8 +45,7 @@ export async function getChart(data, color) {
         },
         legend: {
           align: 'end',
-          usePointStyle: true,
-          pointStyle: circle
+          usePointStyle: true
         }
       },
       scales: {
