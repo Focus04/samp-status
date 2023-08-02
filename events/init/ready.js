b78e47e76b0133f6d47b0d6be92666907e269191
Ogrezeanu Alexandru <alexogrezeanu2004@gmail.com>
2023-08-02 14:43:02 +0300

    .

--- events/init/ready.js
+++ events/init/ready.js
@@ -76,9 +76,9 @@ export default {
           ChartData.value = data.maxPlayersToday;
           ChartData.date = Date.now();
           data.maxPlayersToday = -1;
-          if (!data.days) data.days = [];
-          if (ChartData.value >= 0) data.days.push(ChartData);
-          if (data.days.length > 30) data.days.shift();
+          data.days = [];
+          // if (ChartData.value >= 0) data.days.push(ChartData);
+          // if (data.days.length > 30) data.days.shift();
           await maxPlayers.set(`${server.ip}:${server.port}`, data);
           const channel = await client.channels
             .fetch(interval.channel)
@@ -96,6 +96,6 @@ export default {
             .catch((err) => console.log(err));
         });
       }
-    }, 3600000);
+    }, 60000);
   }
 }
