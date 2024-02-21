d4d03da7263aec57d3d02d11de969ef80afada0c
Ogrezeanu Alexandru <alexogrezeanu2004@gmail.com>
2024-02-21 20:48:17 +0200

    Fixed /stats command

--- commands/stats.js
+++ commands/stats.js
@@ -7,16 +7,17 @@ export default {
     .setDescription(`Tells you how many players are online across all servers.`),
   async execute(interaction) {
     let color = getRoleColor(interaction.guild);
-    const serversResponse = await fetch('http://sam.markski.ar/api/GetAmountServers');
+    const serversResponse = await fetch('http://sam.markski.ar/api/GetGlobalStats');
     const servers = await serversResponse.json();
-    const playersResponse = await fetch('http://sam.markski.ar/api/GetTotalPlayers');
-    const players = await playersResponse.json();
     const infoEmbed = new EmbedBuilder()
       .setColor(color.hex)
       .setTitle('SAMP Live Stats')
       .addFields(
-        { name: 'Online Servers Count', value: servers.toString() },
-        { name: 'Players Across All Servers', value: players.toString() }
+        {
+          name: 'Online (Populated) Servers Count',
+          value: `${servers.serversOnline.toString()} (${servers.serversInhabited.toString()})`
+        },
+        { name: 'Players Across All Servers', value: servers.playersOnline.toString() }
       )
       .setTimestamp();
     interaction.reply({ embeds: [infoEmbed] });
