c0ac56fbe6f6d0b4bdf06847e77f42f2f447e864
Alex <alexogrezeanu2004@gmail.com>
2025-08-12 01:11:34 +0300

    Adds pm2 to auto restart the bot in case of crash

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [{
+    name: "samp-status",
+    script: "index.js",
+    instances: "max",
+    exec_mode: "cluster",
+    autorestart: true,
+    restart_delay: 5000,
+    max_memory_restart: "1G",
+    env: {
+      NODE_ENV: "production"
+    }
+  }]
+}
