ab391f2a14af00c249eac9d9c5447d2d638f0693
Alex <alexogrezeanu2004@gmail.com>
2025-08-12 18:53:12 +0300

    Sends all console logs to discord debug channel

--- utils/sendError.js
+++ utils/sendError.js
@@ -1,12 +1,14 @@
-import { logsChId } from '../config.json' assert { type: 'json' };
+import config from '../config.json' assert { type: 'json' };
 import index from '../index.js';
 
 export function sendWarningLog(msg) {
+  const logsChId = config.logsChId;
   index.client.channels.cache.get(logsChId).send('⚠️ ' + msg);
   console.log(msg);
 }
 
 export function sendInfoLog(msg) {
+  const logsChId = config.logsChId;
   index.client.channels.cache.get(logsChId).send('ℹ️ ' + msg);
   console.log(msg);
 }
