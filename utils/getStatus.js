59d951ba816dba8450096e0a5c0ffdd35d61a5c6
Alex <alexogrezeanu2004@gmail.com>
2025-11-29 00:11:16 +0200

    Makes default interval 5 minutes for non-partner servers

--- utils/getStatus.js
+++ utils/getStatus.js
@@ -86,7 +86,7 @@ export async function getStatus(server, color) {
   }
 
   if (server.banner) {
-    serverEmbed.setFooter({ iconURL: server.banner });
+    serverEmbed.setFooter({ text: '', iconURL: server.banner });
   }
 
   serverEmbed.addFields({ name: '✅ Discover Partner Servers', value: serializedPartnerServers });
