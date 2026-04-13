09aeb8d14aa0a2371e621d87ae55d49494d58550
Alex <alexogrezeanu2004@gmail.com>
2026-04-14 00:15:53 +0300

    fix: Change dogfact API endpoint

--- commands/fun/dogfact.js
+++ commands/fun/dogfact.js
@@ -5,7 +5,7 @@ export default {
     .setName('dogfact')
     .setDescription(`Sends a lovely dog fact.`),
   async execute(interaction) {
-    let response = await fetch('https://dog-api.kinduff.com/api/facts');
+    let response = await fetch('https://dogapi.dog/api/v2/facts');
     const data = await response.json();
     interaction.reply({
       content: '🐶' + data.data[0].attributes.body
